adcfc6b340baa23aac6c76951f48f59227aa0306
cb <christopher@bocki.com>
2022-10-06 00:37:32 +0200

    pv + gv

--- scripts/playerctl_volume.sh
+++ scripts/playerctl_volume.sh
@@ -3,9 +3,9 @@
 main() {
   pvol="$(playerctl --ignore-player="$(printenv IGNOREPLAYERCTL)" \
     metadata --format "{{ volume * 100 }}"| cut -d. -f1 \
-    | awk '{print "PV:"$1"%"}')"
+    | awk '{print "PV: "$1"%"}')"
   gvol="$(amixer -M get Master |grep -Po '\d{0,3}\%')"
-  printf "%s/GV:%s" "$pvol" "$gvol"
+  printf "%s / GV: %s" "$pvol" "$gvol"
 }
 
 main
