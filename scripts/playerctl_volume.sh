a00bd9f11cc066842b377cd09576a1c0f25dbb03
cb <christopher@bocki.com>
2023-01-02 05:02:33 +0100

    global volume via wireplumber/wpctl if available

--- scripts/playerctl_volume.sh
+++ scripts/playerctl_volume.sh
@@ -4,7 +4,11 @@ main() {
   pvol="$(playerctl --ignore-player="$(printenv IGNOREPLAYERCTL)" \
     metadata --format "{{ volume * 100 }}"| cut -d. -f1 \
     | awk '{print "PV: "$1"%"}')"
-  gvol="$(pamixer --get-volume)%"
+  if command -v wpctl >/dev/null; then
+    gvol="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ |perl -pe 's/[[:^digit:]]//g;s/^0//')"
+  else
+    gvol="$(pamixer --get-volume)%"
+  fi
   printf "%s / GV: %s" "$pvol" "$gvol"
 }
 
