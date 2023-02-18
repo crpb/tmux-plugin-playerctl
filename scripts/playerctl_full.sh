6259b72536e60e0e862fe18c0c020786f3d86117
cb <christopher@bocki.com>
2023-02-18 07:01:12 +0100

    blub

--- scripts/playerctl_full.sh
+++ scripts/playerctl_full.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 main() {
-  playerctl --ignore-player="$(printenv IGNOREPLAYERCTL)" metadata --format "{{ artist }} - {{ title }}"
+  playerctl --ignore-player="$(printenv IGNOREPLAYERCTL)" metadata --format "{{ artist }} - {{ album }} - {{ title }}"
 }
 
 main
