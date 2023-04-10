623d28229f6c46fcc951036e4896dec9adbb9d4a
cb <christopher@bocki.com>
2023-04-11 01:21:31 +0200

    typo / ignore

--- scripts/playerctl_player.sh
+++ scripts/playerctl_player.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 get_active_player() {
-  playerctl --ignore-player="$(printenv IGNOREPLAYERCTL)" -li |head -n 1 |sed 's/\..*//g'
+  playerctl --ignore-player="$(printenv IGNOREPLAYERCTL)" -l |head -n 1 |sed 's/\..*//g'
 }
 
 get_active_player
